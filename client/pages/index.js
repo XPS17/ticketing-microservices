96da40e388d0a1cdfc7937c059802b1d85e73482
XPS17 <dkr2217xps@protonmail.com>
2023-01-05 21:29:50 +1100

    refactoring, making reusable API client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,34 +1,17 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
   // axios.get('/api/users/currentuser');
   console.log(currentUser);
 
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    console.log('we are on the server!');
-    // we are on the server!
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-    return data;
-  } else {
-    console.log('we are on the browser!');
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-
-    return data;
-  }
-  return {};
+  return data;
 };
 
 export default LandingPage;
