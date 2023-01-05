0f99b6c3610299d9d3a78e8d716013f2e20749a9
XPS17 <dkr2217xps@protonmail.com>
2023-01-05 21:08:22 +1100

    endpoint calls inside server and from browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,34 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  // axios.get('/api/users/currentuser');
+  console.log(currentUser);
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    console.log('we are on the server!');
+    // we are on the server!
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
+  } else {
+    console.log('we are on the browser!');
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;
